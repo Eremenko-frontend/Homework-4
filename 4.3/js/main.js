db97212940f5805844383bb4e56504c320d090e9
Eremenko-frontend <AleksantdrEremenko1988@gmail.com>
2018-11-20 00:26:42 +0200

    recovery Homework-4.3

--- 4.3/js/main.js
+++ 4.3/js/main.js
@@ -1,72 +1,57 @@
 /* Homework 4.3: Task */
 
-
-var whatsupktaskWriteB = document.getElementById('taskWriteButton');
-
-whatsupktaskWriteB.onclick = function() {
-
-
-    var dynamiTag = document.createElement('li');////створює тег li
-    dynamiTag.className = "task__one-item";//дає клас тегу який я створи раніше
-
-    var checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    checkbox.className = 'task__one-item-checkbox';
-    checkbox.value = 0;// дає значення value даному елементу 0    -значить що задача покищо не виконана
-    dynamiTag.appendChild(checkbox);
-
-
-    var taskDynamicList = document.getElementById('task__list');
-    var text = document.getElementById('task__write-input');//бере текст з input
-    var dynamiTextSpan = document.createElement('span');////створює тег span
-    dynamiTextSpan.className = "task__text-part-item";//дає клас тегу який я створив раніше
-    dynamiTextSpan.appendChild(document.createTextNode(text.value));//вставляє текст з input в span
-
-
-
-    dynamiTag.appendChild(dynamiTextSpan);//вставляє span з текстом в li в якому вже є checkbox
-
-
-
-
-    taskDynamicList.appendChild(dynamiTag);//вставляє текст який було отримано з inpute в створений раніше тег з класом
-};
-
-var taskDeleteB = document.getElementById('taskDeleteButton');
-
-taskDeleteB.onclick = function () {
-    var selectionСheckboxTask = document.getElementsByClassName('task__one-item-checkbox');
-    console.log(selectionСheckboxTask);
-    if (selectionСheckboxTask.checked==true) {
-        alert('Выбран');
+/*Homework 4.3 */
+ var startY = 1;//initial coordinates along the Y-axis
+ var startX = 2;//initial coordinates along the x-axis
+ var myMassive = [
+    [11, 21, 35, 14],
+    [47, 34, 27, 45],
+    [31, 99, 88, 77]
+];
+ function coordinates(oldMassive, coordY, coordX) {
+     var lengthMainMassiv = myMassive.length;//length of the main array
+     var stringLength  = myMassive[0].length;//string length
+     var amountOldElements = lengthMainMassiv * stringLength;//number of elements in the old array
+     var amountOldElementsWrap = amountOldElements * 2;
+     var increase = 0;//starting quantity for bypassing array elements
+     var newMassive = [];//array to record the traversal coordinates
+     newMassive.push([coordY, coordX]);
+     coordX = coordX + 1;
+     while (amountOldElementsWrap > newMassive.length) {
+         increase =  increase + 2;//increase in the number of bypassed elements of the array
+         if (increase > 2) {
+            coordX = coordX + 1;
+        }
+         //downward movement
+         for (var i = increase; i > 0; i--) {
+            newMassive.push([coordY, coordX]);
+            coordY++;
+        }
+        coordY = coordY - 1;
+        coordX = coordX - 1;
+         //move left
+         for (var t = increase; t > 0; t--) {
+            newMassive.push([coordY, coordX]);
+            coordX--;
+        }
+        coordX = coordX + 1;
+         //upward movement
+         for (var j = increase; j > 0; j--) {
+            coordY--;
+            newMassive.push([coordY, coordX]);
+        }
+         //move right
+         for (var p = increase ; p > 0; p--) {
+            coordX++;
+            newMassive.push([coordY, coordX]);
+        }
+     }
+     /* filter part */
+     for (var w = 0; w <= (newMassive.length - 1); w++) {
+        if (newMassive[w][0] < 0 || newMassive[w][0] >= lengthMainMassiv || newMassive[w][1] < 0 || newMassive[w][1] >= stringLength) {
+            delete newMassive[w];
+        }
     }
-    else {
-        alert ('Не выбран');
-    };
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-if (chbox.checked) {
-		alert('Выбран');
-	}
-	else {
-		alert ('Не выбран');
-	}
-
- */
+     return newMassive
+}
+ console.log(coordinates(myMassive, startY, startX));
