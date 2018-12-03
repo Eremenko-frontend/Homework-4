ec5cec83cc7f4ab39961ea69bc166d2196d11d15
Eremenko-frontend <AleksantdrEremenko1988@gmail.com>
2018-12-04 00:22:03 +0200

    Work on the fix in Homework-4.1.1 Part1

--- 4.1.1 Part 1/js/main.js
+++ 4.1.1 Part 1/js/main.js
@@ -2,46 +2,52 @@
 
 var testArrayMyForEach = ['Irina', 'Viktoriya', 'Anastasiya', 'Elena'];
 
-Array.prototype.myforEach = function () {
+Array.prototype.myforEach = function (func) {
   virtualArrayForEach = this;
+  myContext = this;
   var newArrayForEach = [];
 
   for  (var i = 0; i < virtualArrayForEach.length; i++) {
-    newArrayForEach[i] = virtualArrayForEach[i] + ' - Number ' + i;
+    func.call(myContext, virtualArrayForEach, i, newArrayForEach)
   }
 
   return newArrayForEach;
 }
 
 console.log('Array after forEach:');
-console.log(testArrayMyForEach.myforEach())
+console.log(testArrayMyForEach.myforEach(function (virtualArrayForEach, i, newArrayForEach) {
+  newArrayForEach[i] = virtualArrayForEach[i] + ' - Number ' + i;
+}))
 
 /* myMap */
 
 var testArrayMyMap = ['apple', 'cherry', 'orange'];
 
-Array.prototype.myMap =  function () {
+Array.prototype.myMap = function (func) {
 
   virtualLike = this;
-  var newArrayFruits = [];
+  myContext = this;
 
-  for (var i =0; i < virtualLike.length; i++) {
-    newArrayFruits[i] = 'I like ' + virtualLike[i];
-  }
+  for (var i = 0; i < virtualLike.length ; i++) {
 
-  return newArrayFruits;
+    virtualLike[i] = func.call(myContext, virtualLike[i])
+  };
 
+  return virtualLike
 }
 
 console.log('Array after Map:');
-console.log(testArrayMyMap.myMap());
+console.log(testArrayMyMap.myMap(function (whatILike) {
+  whatILike = 'I like ' + whatILike;
+  return whatILike
+}))
 
 /*  mySort  */
 
 var testArrayMySort = [1, -1, 4, 5, 14, 11, 56, 4, 7, 8];
 
 Array.prototype.mySort = function() {
-  newArrayNumbers = this
+  oldArrayNumbers = this
   var newArrayNumbers = [];
 
   for (var i = 0; i < oldArrayNumbers.length; i++) {
@@ -63,20 +69,25 @@ console.log(testArrayMySort.sort());
 
 var testArrayMyFilter = [-3, 7, 1, 2, '127', false, 'table', 45];
 
-Array.prototype.myFilter = function () {
+Array.prototype.myFilter = function (func) {
   virtualCupboard = this
-  var newCustomArray = [];
+  myContext = this
+  var newCustomArray = []
 
-  for (var i = 0; i <virtualCupboard.length; i++ ) {
 
-    if (virtualCupboard[i] > 0) {
+  for (var i = 0; i < virtualCupboard.length ; i++) {
+
+    if (func.call(myContext, virtualCupboard[i])) {
       newCustomArray.push(virtualCupboard[i])
     }
 
   }
 
-  return newCustomArray;
+  return newCustomArray
+
 }
 
 console.log('Array after filter:');
-console.log(testArrayMyFilter.myFilter());
+console.log(testArrayMyFilter.myFilter(function (item) {
+  return item > 0
+}))
